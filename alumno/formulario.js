f92f3183ad4314536916cc09449082bb43c2edd9
IanCeron2 <Ian.cr@outlook.com>
2020-12-25 13:11:06 -0600

    actualizacion al formulario

--- alumno/formulario.js
+++ alumno/formulario.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $('.datepicker').datepicker();
+    var rango = [1994, 2021];
+    $('.datepicker').datepicker({
+        format: "yyyy-mm-dd",
+        autoClose: true,
+        minDate: new Date(1995, 0, 1),
+        maxDate: new Date(2020, 11, 31),
+        yearRange: rango,
+        i18n: {
+            months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+            weekdays: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+            weekdaysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+            weekdaysAbbrev: ["D", "L", "M", "M", "J", "V", "S"]
+        }
+    });
     $('select').formSelect();
     $('.tabs').tabs();
     $("form").validetta({
@@ -7,4 +21,15 @@ $(document).ready(function () {
         bubbleGapTop: 10,
         bubbleGapLeft: -5
     });
+
+    $("#escuela").change(function(){
+        var escuela = $("#escuela option:selected").text();
+        if (escuela == 'Otra'){
+            $("#nombre_escuela").css("display","block");
+        }
+        else{
+            $("#nombre_escuela").css("display","none");
+        }
+      });
+    
 });
