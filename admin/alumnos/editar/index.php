9f26d168df632b84cd34371eb40956ba88e0dcdb
edoomm <edoomm8@gmail.com>
2021-01-01 16:46:25 -0600

    estructura para editar alumnos terminada

--- admin/alumnos/editar/index.php
+++ admin/alumnos/editar/index.php
@@ -14,18 +14,25 @@
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!-- Materialize icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+        /* Con esto se puede cambiar el color del texto de los select
+        input.select-dropdown.dropdown-trigger {
+            color: #ab9eab;
+        } */
+    </style>
 </head>
 
 <body>
-
+    
+    <!-- Top navbar -->
     <header>
-        <!-- Top navbar -->
         <nav>
             <div class="nav-wrapper blue">
                 <ul id="nav-mobile" class="left" style="position: absolute; left: -1%; top: 50%; -webkit-transform: translate(0%, -50%); transform: translate(0%, -50%);">
-                    <a href="../alumnos/"><li><i class="material-icons">chevron_left</i></li></a>
+                    <a href="../../alumnos/"><li><i class="material-icons">chevron_left</i></li></a>
                 </ul>
                 <ul id="nav-mobile" class="right">
                     <div class="right">
@@ -38,37 +45,128 @@
     </header>
 
     <main>
-        <div class="row">
-            <div class="col s12 l4 offset-l4">
-                <div class="card-panel hoverable">
-                    <div class="card-action lighten-1 blue-text">
-                        <h4 style="text-align: center;">Editar información</h4>
-                    </div>
-
-                    <div class="card-content">
-                        <div class="form-field">
-                            <label for="nombre">Nombre</label>
+        <ul class="collapsible expandable">
+            <!-- Datos personales -->
+            <li class="active">
+                <div class="collapsible-header"><i class="material-icons">person</i>Datos personales</div>
+                <div class="collapsible-body">
+                    <div class="form-field">
+                        <div class="row input-field">
+                            <label for="curp">CURP</label>
+                            <input type="text" name="curp" id="curp" disabled>
+                        </div>
+                        <div class="row input-field">
+                            <label for="nombre">Nombre(s)</label>
                             <input type="text" name="nombre" id="nombre">
-                        </div><br>
-                        <div class="form-field">
-                            <label for="apellidopat">Apellido paterno</label>
-                            <input type="text" name="apellidopat" id="apellidopat">
-                        </div><br>
-                        <div class="form-field">
-                            <label for="apellidomat">Apellido materno</label>
-                            <input type="text" name="apellidomat" id="apellidomat">
-                        </div><br>
-        
-                        <div class="form-field">
-                            <button class="btn-large waves-effect waves-dark blue" style="width: 100%;">Guardar</button>
-                        </div><br>
-                        <div class="form-field">
-                            <button class="btn-large waves-effect waves-dark white red-text" style="width: 100%;">Eliminar</button>
-                        </div><br>
+                        </div>
+                        <div class="row">
+                            <div class="col l6 m6 s12 input-field">
+                                <label for="apellidopat">Primer apellido</label>
+                                <input type="text" name="apellidopat" id="apellidopat">
+                            </div>
+                            <div class="col l6 m6 s12 input-field">
+                                <label for="apellidomat">Segundo apellido</label>
+                                <input type="text" name="apellidomat" id="apellidomat">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col s12 m6 l6 input-field">
+                                <label for="fecha-nac">Fecha de nacimiento</label>
+                                <input type="text" class="datepicker" id="fecha-nac" name="fecha-nac">
+                            </div>
+                            <div class="input-field col s12 m6 l6">
+                                <select id="genero">
+                                    <option value="" disabled selected>G&eacute;nero</option>
+                                    <option value="M">Masculino</option>
+                                    <option value="F">Femenino</option>
+                                    <option value="N">No binario</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row input-field">
+                            <label for="correo">Correo electronico</label>
+                            <input type="text" name="correo" id="correo">
+                        </div>
+                        <div class="row">
+                            <div class="col l6 m6 s12 input-field">
+                                <label for="telfijo">Teléfono fijo</label>
+                                <input type="text" name="telfijo" id="telfijo">
+                            </div>
+                            <div class="col l6 m6 s12 input-field">
+                                <label for="telcelular">Teléfono celular</label>
+                                <input type="text" name="telcelular" id="telcelular">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </li>
+            <!-- Datos de domicilio -->
+            <li>
+                <div class="collapsible-header"><i class="material-icons">home</i>Datos de domicilio</div>
+                <div class="collapsible-body">
+                    <div class="form-field">
+                        <div class="row input-field">
+                            <label for="direccion">Dirección</label>
+                            <input type="text" name="direccion" id="direccion">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <!-- Escuela de procedencia -->
+            <li>
+                <div class="collapsible-header"><i class="material-icons">school</i>Escuela de procedencia</div>
+                <div class="collapsible-body">
+                    <div class="form-field">
+                        <div class="row input-field">
+                            <label for="nombreEscuela">Nombre de la escuela</label>
+                            <input type="text" name="nombreEscuela" id="nombreEscuela">
+                        </div>
+                        <div class="input-field col s12 m6 l6">
+                            <select id="bachillerato">
+                                <option value="0" disabled selected>Tipo de bachillerato</option>
+                                <option value="1">Bachillerato general</option>
+                                <option value="2">Bachillerato t&eacute;cnico</option>
+                                <option value="3">Bachillerato en l&iacute;nea</option>
+                            </select>
+                        </div>
+                        <div class="row input-field">
+                            <label for="localidad">Localidad</label>
+                            <input type="text" name="localidad" id="localidad">
+                        </div>
+                        <div class="row input-field">
+                            <label for="promedio">Promedio</label>
+                            <input type="text" name="promedio" id="promedio">
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <!-- Programa academico -->
+            <li>
+                <div class="collapsible-header"><i class="material-icons">book</i>Programa académico</div>
+                <div class="collapsible-body">
+                    <div class="form-field">
+                        <div class="row input-field">
+                            <label for="semestre">Semestre</label>
+                            <input type="text" name="semestre" id="semestre">
+                        </div>
+                        <div class="row input-field">
+                            <label for="programaacad">Programa academico</label>
+                            <input type="text" name="programaacad" id="programaacad">
+                        </div>
+                        <div class="row input-field">
+                            <label for="opción">Opción</label>
+                            <input type="text" name="opción" id="opción">
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="form-field">
+            <button class="btn-large waves-effect waves-dark blue" style="width: 100%;">Guardar</button>
+        </div><br>
+        <div class="form-field">
+            <button class="btn-large waves-effect waves-dark white red-text" style="width: 100%;">Eliminar</button>
+        </div><br>
     </main>
 
     <footer>
@@ -76,9 +174,46 @@
     </footer>
 
     <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
+
+<script>
+    $(document).ready(function(){
+        // Intializing controls
+        $('.collapsible').collapsible();
+        var elem = document.querySelector('.collapsible.expandable');
+        var instance = M.Collapsible.init(elem, {
+            accordion: false
+        });
+        $('select').formSelect();
+
+        // Intializing datepicker
+        var today = new Date();
+        var minFecha = today.getFullYear() - 70;
+        var maxFecha = today.getFullYear() - 10;
+
+        var rango = [minFecha+1, maxFecha];
+        $('.datepicker').datepicker({
+            format: "yyyy-mm-dd",
+            autoClose: true,
+            minDate: new Date(minFecha, 0, 1),
+            maxDate: new Date(maxFecha, 11, 31),
+            yearRange: rango,
+            defaultDate: new Date(2000, 0, 1),
+            setDefaultDate: true,
+            i18n: {
+                months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+                weekdays: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+                weekdaysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+                weekdaysAbbrev: ["D", "L", "M", "M", "J", "V", "S"]
+            }
+        });
+        $('#fecha-nac').val("");
+        M.updateTextFields();
+    });
+</script>
