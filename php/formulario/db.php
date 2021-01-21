531e41e1a2c92cd11911364716a36607873578b6
edoomm <edoomm8@gmail.com>
2021-01-21 11:16:07 -0600

    Resolucion de coflicto en merge

---
+++ php/formulario/db.php
@@ -0,0 +1,16 @@
+<?php
+  function open_database() {
+      $host = "localhost";
+      $user = "root";
+      $pwd = "1234";
+      $db = "sistema_registro";
+
+      $conn = mysqli_connect($host, $user, $pwd, $db);
+      mysqli_set_charset($conn, "utf8");
+
+      if (mysqli_connect_errno())
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+      return $conn;
+  }
+?>
